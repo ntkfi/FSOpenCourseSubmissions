bb918d0bdbe9ecd50f92f71c5e25bda4938773ea
ntkfi <nikokumpulainen@hotmail.fi>
2025-11-06 15:52:43 +0200

    uibuild

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (req, res) => {
   if (req.method === 'POST') {
