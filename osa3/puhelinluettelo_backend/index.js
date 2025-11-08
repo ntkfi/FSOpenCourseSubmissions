e6f6ff5e28e1f0b21f6051c0e6d926754d4c5f8c
ntkfi <nikokumpulainen@hotmail.fi>
2025-11-08 16:31:47 +0200

    osa 3c jatkuuu.....

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const Person = require('.models/person')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -101,15 +101,18 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -121,18 +124,17 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({
           error: validationError
         })
+      } else {
+        const person = new Person({
+          name: body.name,
+          number: body.number
+          // id: generateId()
+        })
+        person.save().then(savedPerson => {
+          res.json(savedPerson)
+        })
       }
     })
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
- // id: generateId()
-  })
-
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -142,14 +144,30 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
 // TODO:
-// 1. korjaa POST. validationerror lähinnä
-// 2. korjaa id:llä haku
 // 3. korjaa poisto
 // 4. korjaa /info sivu
 // 5. poista lokaali persons-objekti
