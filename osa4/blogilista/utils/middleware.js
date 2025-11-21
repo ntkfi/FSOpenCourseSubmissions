b5b228a1fc7047382c47cf70f66ccd8bbd7fb948
ntkfi <ntkumpul@student.jyu.fi>
2025-11-21 19:18:34 +0200

    osa 4d jatka 4.21

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,3 +1,13 @@
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -23,4 +33,4 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { unknownEndpoint, errorHandler }
+module.exports = { tokenExtractor, unknownEndpoint, errorHandler }
